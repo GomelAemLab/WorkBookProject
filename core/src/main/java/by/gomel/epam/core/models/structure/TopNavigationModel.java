693aa687197590336620ef5f0e210245f6c2b55d
Pavel_Prymachou <pavel_prymachou@epam.com>
2019-05-27 15:42:33 +0300

    EPM-WBC-6 1. Static content was added; 2. "body-topnav" script is corrected(page title); 3. "TopNavigationModel" has been simplified.

--- core/src/main/java/by/gomel/epam/core/models/structure/TopNavigationModel.java
+++ core/src/main/java/by/gomel/epam/core/models/structure/TopNavigationModel.java
@@ -1,35 +1,38 @@
 package by.gomel.epam.core.models.structure;
 
-import by.gomel.epam.core.utils.PageNavigationUtil;
 import com.day.cq.wcm.api.Page;
 import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 @Model(adaptables = {SlingHttpServletRequest.class})
 public class TopNavigationModel {
+
+    private static final String HOME_ROOT_PAGE_PATH = "/content/aem-workbook/en";
 
     @Inject
     private SlingHttpServletRequest request;
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    private static final String SECTION_PATH_REGEX = "(?=/)(/([a-z0-9-]*))(?=/)";
-
-    private static final int FIRST_SITE_LEVEL_INDEX = 0;
-    private static final int SECOND_SITE_LEVEL_INDEX = 1;
-
     @PostConstruct
     protected void init() {
         logger.info("Initialisation of model: {}", getClass());
+    }
+
+    /**
+     * Returns path of the root page.
+     * @return
+     */
+    public String getHomeRootPagePath() {
+        return HOME_ROOT_PAGE_PATH;
     }
 
     /**
@@ -38,14 +41,12 @@ public class TopNavigationModel {
      * @return List of the pages.
      */
     public List<Page> getRootPages() {
-
-        Pattern pattern = Pattern.compile(SECTION_PATH_REGEX);
-        Matcher matcher = pattern.matcher(request.getRequestPathInfo().getResourcePath());
-        List<String> pathList = new ArrayList<>();
-        while (matcher.find()) {
-            pathList.add(matcher.group());
+        Resource rootResource = request.getResourceResolver().resolve(HOME_ROOT_PAGE_PATH);
+        List<Page> pages = new LinkedList<>();
+        for (Resource childPage : rootResource.getChildren()) {
+            Page page = childPage.adaptTo(Page.class);
+            pages.add(page);
         }
-        String rootPath = pathList.get(FIRST_SITE_LEVEL_INDEX) + pathList.get(SECOND_SITE_LEVEL_INDEX);
-        return PageNavigationUtil.getPagesByPath(request.getResourceResolver(), rootPath);
+        return pages;
     }
 }
